98a57f6dff524b685ca138e6defc30e11a0a9913
Mohammed0303 <109842106+Mohammed0303@users.noreply.github.com>
2022-07-28 15:15:18 +0530

    Add files via upload

---
+++ TaxCalculator.py
@@ -0,0 +1,18 @@
+income = int(input("Enter your income here: "))
+tax_payable = 0
+print(f"Given income is {income}")
+
+if income <= 10000:
+    tax_payable = 0
+elif income <= 20000:
+    x = income-10000
+
+    tax_payable = x * 10 / 100
+else:
+    tax_payable = 0
+
+    tax_payable = 10000 * 10 / 100
+
+    tax_payable += (income - 20000) * 20 / 100
+
+print(f"Total tax to pay is {tax_payable}")
